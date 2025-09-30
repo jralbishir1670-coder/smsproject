9b65d9d20ef08224282faeaf60be38777002c572
Usman Albishir <143898229+Jrusman@users.noreply.github.com>
2025-09-30 16:02:13 +0100

    Heading

--- static/js/script.js
+++ static/js/script.js
@@ -19,6 +19,41 @@ function closeModal(id) {
 // (function () {
 //   emailjs.init("UEwG4jSXgJOjd14un"); // replace with your public key from EmailJS
 // })();
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("contact-form");
+
+//   if (form) {
+//     form.addEventListener("submit", function (event) {
+//       event.preventDefault();
+
+//       emailjs.sendForm("service_af1uun8", "template_ghrcm5n", form).then(
+//         () => {
+//           alert("Message sent successfully ✅");
+//         },
+//         (error) => {
+//           console.error("Failed to send message ❌", error);
+//           alert("Oops! Something went wrong.");
+//         }
+//       );
+//     });
+//   }
+// });
+
+// function sendMail() {
+//   let parms = {
+//     name : document.getElementById("name").value,
+//     email : document.getElementById("email").value,
+//     subject: document.getElementById("subject").value,
+//     message : document.getElementById("message").value
+//   }
+
+//   emailjs.sendForm("service_1tcv6fj", "service_1tcv6fj", parms).then(alert("Message sent successfully ✅"));
+// }
+
+// (function () {
+//   emailjs.init("UEwG4jSXgJOjd14un"); // replace with your public key from EmailJS
+// })();
 // document.addEventListener("DOMContentLoaded", function () {
 //   const form = document.getElementById("contact-form");
 
